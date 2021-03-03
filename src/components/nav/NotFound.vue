b4990e9d1c3fafc75c0aa1b634b9cc4b0f9d1673
jesspsilva <jps936@hotmail.com>
2021-03-03 16:43:18 +0000

    Add not found component

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Page not found! <router-link to="/teams">Go to homepage</router-link></h2>
+</template>
+
+<style scoped>
+h2 {
+  text-align: center;
+  font-size: 1.4rem;
+}
+
+a {
+  text-decoration: none;
+}
+</style>
