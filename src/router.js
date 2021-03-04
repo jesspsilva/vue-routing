9c2ca96d89adf5a6147a15f3df827db6b6d89633
Jéssica <jps936@hotmail.com>
2021-03-04 20:12:25 +0000

    Organize route files

---
+++ src/router.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    { 
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components:  {
+        default: TeamsList,
+        footer: TeamsFooter
+      }, 
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
+      ] 
+    },
+    { path: '/users', 
+      components: {
+        default: UsersList,
+        footer: UsersFooter
+      }
+    },
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+    return {
+      left: 0, top: 0
+    };
+  }
+});
+
+export default router;
